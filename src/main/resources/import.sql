7dd4ad6c04089e48756575214a4c4a68870886ac
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-01-20 19:39:56 -0300

    class client and database seeding.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('2000-12-13', 3, 55000.0, '123.456.789-00', 'Ana Maria Braga');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1988-7-2', 1, 97000.0, '777.892.345-12', 'Muhamed Salim Mussalem');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1977-2-27', 9, 25000.0, '003.977.383-45', 'Joshua Cohen');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1968-10-11', 15, 35000.0, '005.423.567-11', 'Hosana Chucram Haram');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1933-01-11', 2, 89000.0, '001.993.597-35', 'Sigmund Froid' );
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1977-08-19', 14, 215000.0, '001.944.567-43','Xing Ping Ling' );
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1997-09-04', 2, 26000.0, '005.234.567-11', 'José Morales Ximenez');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('2002-09-04', 1, 20000.0, '004.676.321-00', 'Pierre Louvre Duvivieur');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1945-03-10', 5, 27000.0, '001.923.789-10', 'Ray Charlles Junior');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1968-11-30', 18, 21500.0, '018.150.054-89', 'Zulah Timbó Zemembaba');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('2000-01-01', 4, 30000.0, '789.234.985-12','Targgino Pieruggi Santini Grosso');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1998-08-19', 2, 54000.00, '004.777.998-11', 'Gabriella Lirio Ayres');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('2002-09-04', 2, 60000.00, '004.778.999-12', 'Julia Lirio Ayres');
+INSERT INTO tb_client(birth_date, children, income, cpf, name) VALUES ('1968-01-11', 2, 70000.00, '005.669.704-12', 'Eliane de Almeida Lirio Ayres');
