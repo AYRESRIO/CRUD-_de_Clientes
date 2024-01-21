cb61b8e84f29c83dd790825cc5e0187de014c4cb
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-01-21 09:05:03 -0300

    GET Clients paged.

--- src/main/java/com/devsuperior/CRUD_de_Clientes/controllers/ClientController.java
+++ src/main/java/com/devsuperior/CRUD_de_Clientes/controllers/ClientController.java
@@ -1,15 +1,16 @@
 package com.devsuperior.CRUD_de_Clientes.controllers;
 
 import com.devsuperior.CRUD_de_Clientes.dto.ClientDTO;
-import com.devsuperior.CRUD_de_Clientes.entities.Client;
-import com.devsuperior.CRUD_de_Clientes.repositories.ClientRepository;
 import com.devsuperior.CRUD_de_Clientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,5 +22,9 @@ public class ClientController {
     public ClientDTO findById(@PathVariable Long id){
     ClientDTO dto = service.findById(id);
     return dto;
+    }
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
